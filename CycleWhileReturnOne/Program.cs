e5b9229eedf888df6131afa153f9d20d63f1d24b
zenin5479 <zenin5479@gmail.com>
2026-03-16 15:04:14 +0300

    Update 0.5 Обновлен метод. Проведен рефакторинг

--- CycleWhileReturnOne/Program.cs
+++ CycleWhileReturnOne/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 
-namespace CycleWhileReturnOne
+namespace ConsoleExperiment
 {
-   internal class Program
+   class Program
    {
       static void Main()
       {
-         Console.WriteLine("Hello World!");
+         double inputValue = 1024;
+         //double result = CalculateConsole(inputValue);
+         //Console.WriteLine(result);
+
+         Calculate(inputValue);
+
+         //long iterations = 0;
+         //while (true)
+         //{
+         //   iterations++;
+         //   double result = inputValue * iterations;
+         //   Console.WriteLine("Итерация: {0}", result);
+         //}
+      }
+
+      private static void Calculate(double input)
+      {
+         // Используем long, чтобы избежать переполнения int
+         long iterations = 0;
+         // Бесконечный цикл
+         while (true)
+         {
+            iterations++;
+
+            double result = input * iterations;
+            Console.WriteLine("Итерация: {0}", result);
+         }
+      }
+
+      private static double CalculateConsole(double input)
+      {
+         // Счётчик итераций
+         int iterationCount = 0;
+         // Бесконечный цикл
+         while (true)
+         {
+            // Увеличиваем счётчик на каждой итерации
+            iterationCount++;
+            double result = input * iterationCount;
+            Console.Write(result);
+            Console.WriteLine("Итерация: {0}", iterationCount);
+            return result;
+         }
       }
    }
 }
