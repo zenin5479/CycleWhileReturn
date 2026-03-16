b07e9ff8e5e25e2f443ef17935d6a067d96565e3
zenin5479 <zenin5479@gmail.com>
2026-03-16 14:57:00 +0300

    Добавьте файлы проекта.

---
+++ CycleWhileReturnTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CycleWhileReturnTwo
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
