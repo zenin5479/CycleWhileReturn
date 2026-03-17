8053b245475f65d000e45db5b549f6d6b6f8a450
zenin5479 <zenin5479@gmail.com>
2026-03-17 12:42:44 +0300

    Update 2.9 Обновлен метод. Проведен рефакторинг

--- CycleWhileReturnOne/Program.cs
+++ CycleWhileReturnOne/Program.cs
@@ -22,15 +22,6 @@ namespace CycleWhileReturnOne
 
       private static void Calculate(double input)
       {
-         // Используем long, чтобы избежать переполнения int
-         long iterations = 0;
-         // Бесконечный цикл
-         while (true)
-         {
-            iterations++;
-            double result = input * iterations;
-            Console.WriteLine("Итерация: {0}", result);
-         }
       }
    }
 }
