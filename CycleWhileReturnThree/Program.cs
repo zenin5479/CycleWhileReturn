13b01da0cccf7cf3e5e80b6844bab82de03efc64
zenin5479 <zenin5479@gmail.com>
2026-03-18 09:41:03 +0300

    Update 3.8 Обновлен метод. Проведен рефакторинг

---
+++ CycleWhileReturnThree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CycleWhileReturnThree
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
