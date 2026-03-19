837956aba3a3cdd987d3bf198268801e5fd4113d
zenin5479 <zenin5479@gmail.com>
2026-03-19 11:29:27 +0300

    Update 7.2 Обновлен метод. Проведен рефакторинг

--- CycleWhileReturnFour/Program.cs
+++ CycleWhileReturnFour/Program.cs
@@ -1,12 +1,20 @@
 ﻿// Вариант с передачей результата в другой метод на каждой итерации
 
+using System;
+
 namespace CycleWhileReturnFour
 {
    internal class Program
    {
       static void Main()
       {
+         double inputValue = 1024;
 
+         // Перебираем результаты каждой итерации
+         foreach (var result in CalculateConsole(inputValue))
+         {
+            Console.WriteLine($"Полученный результат: {result}");
+         }
       }
    }
 }
