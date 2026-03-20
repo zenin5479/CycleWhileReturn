fa366322900293281e55f9c8ca0b495d92eb9e2a
zenin5479 <zenin5479@gmail.com>
2026-03-20 17:27:20 +0300

    Update 9.5 Обновлен метод. Проведен рефакторинг

---
+++ CycleWhileReturnFive/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CycleWhileReturnFive
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
